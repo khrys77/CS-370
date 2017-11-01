ddc11de6f2d8c9c22aee9d27f257a97f157786db
khrys77 <33103940+khrys77@users.noreply.github.com>
2017-11-01 19:06:17 -0400

    Add files via upload

---
+++ player.h
@@ -0,0 +1,46 @@
+
+/* 
+ * File:   player.h
+ * Author: Khrys' Pc
+ *
+ * Created on October 18, 2017, 9:45 PM
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+class player {
+public:
+    player();
+    void set_xpos (int);
+    void set_ypos (int);
+    void set_pname (string);
+    void set_resource (int,int,int,int);
+    void set_pscore (int);
+    void set_num_of_players (int);
+    int getxpos();
+    int getypos();
+    int get_pscore();
+    void get_presource();
+    int get_num_of_players();
+    string get_pname();
+    void save();
+    void load();
+
+private:
+    int xpos;
+    int ypos;
+    int presource;
+    int pscore;
+    int num_of_players;
+
+};
+
+#endif /* PLAYER_H */
+
